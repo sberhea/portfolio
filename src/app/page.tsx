f8ba096162782aca28cb60301b1810c2fc9b1efc
Selam Berhea <sberhea@gmail.com>
2025-09-10 16:47:50 -0600

    chore: refactor nav bar and adjust font/color styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,18 +4,9 @@ import Hero from "@/components/Hero";
 export default function HomePage() {
   return (
     <main className="">
-       <header className="flex justify-between items-center px-6 py-4">
-        {/* Left side - Name and Email */}
-        <div className="flex flex-col">
-          <h1 className="text-2xl font-semibold">selam berhea</h1>
-          <p className="text-lg">selam.work@proton.me</p>
-        </div>
-        
        {/* Navbar */}
         <Navbar />
-
-        <div className="flex"></div>
-      </header>
+  
 
       {/* Page content */}
       <div className="max-w-4xl py-12">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,26 +2,32 @@
 
 const Navbar = () => {
   return (
-    <nav className="flex justify-evenly gap-x-12">
-          <a 
-            href="#design" 
-            className="px-3 py-1 text-xl font-medium bg-blue-100 hover:bg-yellow-200 transition-colors duration-200"
-          >
-            Design
-          </a>
-          <a 
-            href="#writing" 
-            className="px-3 py-1 text-xl font-medium  bg-blue-100 hover:bg-yellow-200 transition-colors duration-200"
-          >
-            Writing
-          </a>
-          <a 
-            href="#dev" 
-            className="px-3 py-1 text-xl font-medium bg-blue-100 hover:bg-yellow-200 transition-colors duration-200"
-          >
-            Dev
-          </a>
-        </nav>
+    <header className="flex justify-between items-center px-6 py-4">
+        <div className="flex flex-col">
+            <h1 className="text-2xl font-semibold">selam berhea</h1>
+            <p className="text-lg">selam.work@proton.me</p>
+            </div>
+        <nav className="flex justify-evenly gap-x-12">
+            <a 
+                href="/design" 
+                className="px-3 py-1 text-xl font-medium bg-navActive hover:bg-navHover transition-colors duration-200"
+            >
+                Design
+            </a>
+            <a 
+                href="/writing" 
+                className="px-3 py-1 text-xl font-medium  bg-navActive hover:bg-navHover transition-colors duration-200"
+            >
+                Writing
+            </a>
+            <a 
+                href="/dev" 
+                className="px-3 py-1 text-xl font-medium bg-navActive hover:bg-navHover transition-colors duration-200"
+            >
+                Dev
+            </a>
+            </nav>
+    </header>
   );
 };
 
