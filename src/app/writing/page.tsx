2ade3381f21b396239a9e621886af1cdd8b55741
Selam Berhea <sberhea@gmail.com>
2025-09-10 16:46:32 -0600

    chore: create design, dev, and writing page files

---
+++ src/app/writing/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from "@/components/Navbar";
+
+export default function HomePage() {
+  return (
+    <main className="">
+       {/* Navbar */}
+        <Navbar />
+
+      <div className="">
+
+      </div>
+
+    </main>
+  )
+}
