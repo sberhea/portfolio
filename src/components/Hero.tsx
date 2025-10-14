250e7cbff85ad793f6d0b6d709f07f66f4b9a13e
Selam Berhea <sberhea@gmail.com>
2025-10-14 11:48:33 -0600

    chore: adjust hero text for mobile

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,8 @@
 const Hero = ({}) => {
   return (
-    <section className="p-8">
+    <section className="p-4 md:p-8">
       {/* Bio */}
-    <div className="px-8 tracking-normal font-stretch-expanded space-y-4 text-3xl leading-relaxed text-black italic">
+    <div className="px-4 md:px-8 tracking-normal font-stretch-expanded space-y-3 md:space-y-4 text-xl md:text-2xl leading-relaxed text-black italic">
 
       <p className="">
         Selam is a <span className="hover:bg-lime-300 pr-0.5"> designer</span>, <span className="hover:bg-lime-300 pr-0.5">engineer</span>, and <span className="hover:bg-lime-300 pr-0.5">writer</span>. 
