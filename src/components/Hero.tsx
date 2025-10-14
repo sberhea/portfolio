90e22c4c9e56319724366a03d7f3416b59fb4744
Selam Berhea <sberhea@gmail.com>
2025-10-13 23:27:30 -0600

    chore: edit hero text and padding

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ const Hero = ({}) => {
   return (
     <section className="p-8">
       {/* Bio */}
-    <div className="mx-auto px-8 mt-10 tracking-normal font-stretch-expanded space-y-4 text-3xl leading-relaxed text-black italic">
+    <div className="px-8 tracking-normal font-stretch-expanded space-y-4 text-3xl leading-relaxed text-black italic">
 
       <p className="">
         Selam is a <span className="hover:bg-lime-300 pr-0.5"> designer</span>, <span className="hover:bg-lime-300 pr-0.5">engineer</span>, and <span className="hover:bg-lime-300 pr-0.5">writer</span>. 
@@ -13,9 +13,6 @@ const Hero = ({}) => {
       <p>
         During that time, she discovered a love of UX/UI design that she has
         brought to clients working in education, transporation, and wellness. 
-      </p>
-      <p>
-        Selam has a life-long passion of writing and worked as a journalist early in her career. 
       </p>
       </div>
     </section>
